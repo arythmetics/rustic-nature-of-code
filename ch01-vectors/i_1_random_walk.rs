0c0e16096f753b76939d3a341269bd96ba0811e3
Arya <arya@Aryas-Mac.local>
2022-01-06 11:12:15 -0500

    intro.1 random walk

---
+++ ch01-vectors/i_1_random_walk.rs
@@ -0,0 +1,58 @@
+use nannou::prelude::*;
+
+fn main() {
+    nannou::app(model).update(update).run();
+}
+
+struct Model 
+{
+    x: f32,
+    y: f32,
+}
+
+fn model(app: &App) -> Model {
+    let x = 0.0;
+    let y = 0.0;
+    
+    let _window = app.new_window().view(view).build().unwrap();
+    Model 
+    { 
+        x,
+        y, 
+    }
+}
+
+fn update(_app: &App, model: &mut Model, _update: Update) 
+{
+    let choice = random_range(1,5);
+
+    if choice == 1
+    {
+        model.x = model.x + 1.0;
+    }
+    else if choice == 2
+    {
+        model.x = model.x - 1.0;
+    }
+    else if choice == 3
+    {
+        model.y = model.y + 1.0
+    }
+    else if choice == 4
+    {
+        model.y = model.y - 1.0
+    };
+}
+
+fn view(app: &App, model: &Model, frame: Frame) {
+    let draw = app.draw();
+    draw.background().color(PLUM);
+
+    draw.ellipse()
+        .x_y(model.x, model.y)
+        .w_h(5.0, 5.0)
+        .color(STEELBLUE)
+        .stroke(BLACK);
+    
+    draw.to_frame(app, &frame).unwrap();
+}
